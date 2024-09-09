d76d702cf3089b4235c2b770f100cb227013d00c
musash1 <luis.gjokaj@gmail.com>
2024-09-09 22:45:27 +0000

    zap

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,37 @@
 const std = @import("std");
+const zap = @import("zap");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var listener = zap.HttpListener.init(.{
+        .port = 3000,
+        .on_request = on_request,
+        .log = true,
+    });
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    try listener.listen();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
 
-    try bw.flush(); // don't forget to flush!
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
+}
+
+fn on_request(r: zap.Request) void {
+    if (r.path) |the_path| {
+        std.debug.print("PATH: {s}\n", .{the_path});
+    }
+
+    if (r.query) |the_query| {
+        std.debug.print("QUERY: {s}\n", .{the_query});
+    }
+    r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>") catch return;
 }
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    defer list.deinit();
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
